b02abdfa3c5551cf55977e6d04c2bc6fcf8655fc
Mutowu <161786432+Mutowu@users.noreply.github.com>
2025-01-23 16:03:16 +0800

    Add files via upload

---
+++ BridAllPic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridAllPic : MonoBehaviour
+{
+    public List<GameObject> list_allBirdPic = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
